c5a1208be1f300ec5f7444a399c38d421577d3bf
rlx <rramosp@unal.edu.co>
2019-09-03 08:25:46 -0500

    added setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rlxmoocapi',
+      version='0.1',
+      description='API utils to rlx MOOC framework',
+      author='raul ramos',
+      author_email='raul.ramos@udea.edu.co',
+      url='https://github.com/rramosp/rlxmoocapi',
+      packages=['numpy'],
+     )
