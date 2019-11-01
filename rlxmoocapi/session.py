b3c88eda905f43e8d2fc85a2679291e0d97c1cf8
Raul Ramos-Pollan <rlx@Rauls-MacBook-Pro.local>
2019-11-01 14:37:06 -0500

    fixed session

--- rlxmoocapi/session.py
+++ rlxmoocapi/session.py
@@ -1,4 +1,4 @@
-import requests, json, getpass, inspect
+import requests, json, getpass, inspect, pickle, codecs
 from IPython.core.display import display, HTML
 
 class Session:
@@ -9,9 +9,10 @@ class Session:
 
     def do(self, request_function, url, data=None, loggedin_required=True):
         assert not loggedin_required or self.token is not None, "must login first"
-        resp = request_function(self.endpoint+"/"+url, json=data, 
+        resp = request_function(self.endpoint+"/api/"+url, json=data, 
                              headers={'Content-Type':'application/json',
                                       'Mooc-Token': self.token})
+
         if resp.status_code!=200:
             c = eval(resp.content)
             if "traceback" in c:
@@ -56,14 +57,32 @@ class Session:
 
         return self
         
-    def create_user(self, user_id, pwd, user_name):
-        data = {"user_id": user_id, "user_name": user_name, "user_pwd": pwd}
+    def create_user(self, user_id, pwd, user_name, user_email):
+        data = {"user_id": user_id, "user_name": user_name, "user_pwd": pwd, "user_email": user_email}
         self.do_post("users", data)
 
     def get_user(self, user_id):
         resp = self.do_get("users/%s"%user_id)
         if resp.status_code==200:
             return eval(resp.content.decode())
+
+    def pwd_change(self, user_id=None):
+        user_id = user_id if user_id is not None else self.user_id
+        resp = self.do_get("users/%s/request_pwd_change"%user_id)
+        print ("check your email for password change code\n\n")
+        code = input("password change code: ")
+        old_pwd =  getpass.getpass("old password:         ")
+        new_pwd1 = getpass.getpass("new password:         ")
+        new_pwd2 = getpass.getpass("new password (again): ")
+
+        if new_pwd1!=new_pwd2:
+            raise ValueError("new password does not match")
+
+        data = {"code": code, "old_pwd": old_pwd, "new_pwd": new_pwd1}
+        resp = self.do_post("users/%s/pwd_change"%user_id, data)
+        if resp.status_code==200:
+            return eval(resp.content.decode())
+
 
     def delete_user(self, user_id):
         self.do_delete("users/%s"%user_id)
@@ -101,10 +120,17 @@ class Session:
         data = {"user_id": user_id, "course_id": course_id, "start_date": start_date}
         self.do_post("users/%s/courses"%user_id, data)
         
-    def delete_user_course(self, course_id, user_id):
+    def delete_user_course(self, course_id, user_id, delete_grades_and_submissions=False):
         user_id = user_id if user_id is not None else self.user_id
-        self.do_delete("users/%s/courses/%s"%(user_id, course_id))
-        
+        data = {"delete_grades_and_submissions": str(delete_grades_and_submissions)}
+        self.do_delete("users/%s/courses/%s"%(user_id, course_id), data=data)
+
+    def get_user_courses(self, user_id=None):
+        user_id = user_id if user_id is not None else self.user_id
+        resp = self.do_get("users/%s/courses"%(user_id))
+        if resp.status_code==200:
+            return eval(resp.content.decode())
+
     def get_user_course(self, course_id, user_id=None):
         user_id = user_id if user_id is not None else self.user_id
         resp = self.do_get("users/%s/courses/%s"%(user_id, course_id))
@@ -117,6 +143,14 @@ class Session:
         if resp.status_code==200:
             return eval(resp.content.decode())["result"]==str(True)
 
+    def get_user_course_gradetree(self, course_id=None, user_id=None):
+        course_id = course_id if course_id is not None else self.course_id
+        user_id = user_id if user_id is not None else self.user_id
+        resp = self.do_get("users/%s/courses/%s/grade_tree"%(user_id, course_id))
+        if resp.status_code==200:
+            r = eval(resp.content.decode())
+            return r
+                
     def set_grader(self, course_id, lab_id, task_id, 
                    grader_source, grader_function_name,
                    source_functions_names, source_variables_names):
@@ -146,6 +180,20 @@ class Session:
         if resp.status_code==200:
             return json.loads(resp.content.decode())
 
+    def get_submissions(self, course_id=None, lab_id=None, task_id=None, user_id=None, include_details=False):
+        user_id = user_id if user_id is not None else self.user_id
+        course_id, lab_id = self.default_course_lab(course_id, lab_id)
+        data = {"include_details": str(include_details) }
+        resp = self.do_get("users/%s/courses/%s/labs/%s/tasks/%s/submissions"%(user_id, course_id, lab_id, task_id),
+                           data=data)
+        if resp.status_code==200:
+            return json.loads(resp.content.decode())['Items']
+
+    def delete_submissions(self, course_id=None, lab_id=None, task_id=None, user_id=None):
+        user_id = user_id if user_id is not None else self.user_id
+        course_id, lab_id = self.default_course_lab(course_id, lab_id)
+        self.do_delete("users/%s/courses/%s/labs/%s/tasks/%s/submissions"%(user_id, course_id, lab_id, task_id))
+
     def submit_task(self, namespace, course_id=None, lab_id=None, task_id=None, user_id=None,
                     display_html=True):
         """
@@ -156,7 +204,9 @@ class Session:
 
         source = self.get_grader_source_names(course_id, lab_id, task_id)
         functions = {f: inspect.getsource(namespace[f]) for f in source['source_functions_names']}
-        variables = {v: namespace[v] for v in source['source_variables_names']}
+        # variables = {v: str(namespace[v]) for v in source['source_variables_names']}
+        variables = codecs.encode(pickle.dumps({i:namespace[i] for i in source['source_variables_names']}), "base64").decode()
+
         submission_content = { 'source_functions': functions,
                                'source_variables': variables}
 
@@ -164,22 +214,67 @@ class Session:
         resp = self.do_post("users/%s/courses/%s/labs/%s/tasks/%s"%(user_id, course_id, lab_id, task_id), data)
         if resp.status_code==200:
            r = eval(resp.content.decode()) 
+           gmsg = r["message"].strip()
+           if len(gmsg)>0:
+              gmsg = "<pre>----- grader message -------</pre>%s<pre>----------------------------</pre>"%gmsg
+          
            if display_html:
                 s = """
-                <h2>%s submitted</h2>
-                <p/><p/>
-                <h3><font color="blue">your grade is %s</font></h3>
-                <p/><p/>
-                %s
+                <b>%s submitted.</b> <b><font color="blue">your grade is %s</font></b> 
+                <p/>%s
                 <p/><p/>
                 <div style="font-size:10px"><b>SUBMISSION CODE</b> %s</div>
 
-                """%("task_01", str(r["grade"]), r["message"], r["submission_stamp"])
+                """%(task_id, str(r["grade"]), gmsg, r["submission_stamp"])
                 display(HTML(s))               
            return r
 
+    def print_grade_tree(self, course_id=None, user_id=None):
+        gt = self.get_user_course_gradetree(course_id, user_id)
+        ccu = Course(self.course["user_course_spec"])
+        r = "+-------+----------+----------------------------+\n"
+        r += "+ grade + part id  + description                +\n"
+        r += "+-------+----------+----------------------------+\n"
+        r += "%8.2f TOTAL GRADE   %s\n\n"%(gt["grade"], ccu.spec["course_description"])
+        for k in sorted(gt["labs"].keys()):
+            lab = ccu.get_lab(k)
+            r += "+-------+----------+----------------------------+\n"
+            r += "%8.2f %-10s %s\n"%(gt["labs"][k]["grade"], k, lab["name"])
+            r += "+-------+----------+----------------------------+\n"
+            for t in sorted(gt["labs"][k]["tasks"].keys()):
+                _, task = ccu.get_labtask(k,t)
+                r+="%8.2f %-10s %s\n"%(gt["labs"][k]["tasks"][t], t, task["name"])
+            r += "\n"
+        print (r)
+
     def run_grader_locally(self, grader_function_name, source_functions_names, source_variables_names, namespace):
         functions = {f: eval("inspect.getsource(%s)"%f, namespace) for f in source_functions_names}
-        variables = {v: namespace[v] for v in source_variables_names}
+
+        # serialize and unserialize variables with pickle to simulate 
+        # sending string through http
+        dvars = codecs.encode(pickle.dumps({i:namespace[i] for i in source_variables_names}), "base64").decode()
+        variables = pickle.loads(codecs.decode(dvars.encode(), "base64"))
         return namespace[grader_function_name](functions, variables)
 
+    def make_backup(self):
+        return self.do_get("make_backup")
+
+class Course:
+
+    def __init__(self, spec):
+        self.spec = spec
+        self.course_id = spec["course_id"]
+
+    def get_lab(self, lab_id):
+        for lab in self.spec["labs"]:
+            if lab["lab_id"]==lab_id:
+                return lab
+        assert False, "lab %s, in course %s not found"%(lab_id, self.course_id)
+
+    def get_labtask(self, lab_id, task_id):
+        for lab in self.spec["labs"]:
+            if lab["lab_id"]==lab_id:
+                for task in lab["tasks"]:
+                    if task["task_id"]==task_id:
+                        return lab, task
+        assert False, "lab/task %s/%s, in course %s not found"%(lab_id, task_id, self.course_id)
